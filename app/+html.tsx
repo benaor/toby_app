ab291417ccef7e7f21d5ba287b8b6e631ebb8e1f
Benaor <benjamingirard25@gmail.com>
2024-06-24 01:05:32 +0200

    styles: add noto-sans for web

--- app/+html.tsx
+++ app/+html.tsx
@@ -15,6 +15,10 @@ export default function Root({ children }: PropsWithChildren) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link
+          href="https://fonts.cdnfonts.com/css/noto-sans"
+          rel="stylesheet"
+        />
 
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
