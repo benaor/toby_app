afc9c1304d6f41bdb2c1f92787cb576f15924e5b
Benaor <benjamingirard25@gmail.com>
2024-06-27 23:12:34 +0200

    build: import Noto Sans font for android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,7 +14,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    "Noto Sans": require("../assets/fonts/NotoSans-Variable.ttf"),
   });
 
   useEffect(() => {
