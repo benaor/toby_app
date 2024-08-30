5aca95a7fdd8abb4313164ee860f138a69ce7b5f
Benaor <benjamingirard25@gmail.com>
2024-08-30 21:57:18 +0200

    feat(location): add location to event (UI)

--- src/modules/events/ui/screens/AddLocationModal/AddLocationModal.controller.tsx
+++ src/modules/events/ui/screens/AddLocationModal/AddLocationModal.controller.tsx
@@ -1,3 +1,14 @@
+import { useRouter } from "expo-router";
+import { useCallback } from "react";
+
 export const useAddLocationModal = () => {
-  return {};
+  const { back } = useRouter();
+
+  const saveLocation = useCallback(() => {
+    back();
+  }, [back]);
+
+  return {
+    saveLocation,
+  };
 };
--- src/modules/events/ui/screens/AddLocationModal/AddLocationModal.view.tsx
+++ src/modules/events/ui/screens/AddLocationModal/AddLocationModal.view.tsx
@@ -1,11 +1,22 @@
+import { Button } from "@components/Button";
 import { ModalLayout } from "@components/ModalLayout";
+import { TextInput } from "@components/TextInput";
 import { createStyleSheet } from "@themes/createStyleSheet";
 import { View } from "react-native";
+import { useAddLocationModal } from "./AddLocationModal.controller";
 
 export const AddLocationModal = () => {
+  const { saveLocation } = useAddLocationModal();
   return (
     <ModalLayout title="Ajouter un lieu">
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <TextInput label="Nom du lieu" variant="filled" />
+        <TextInput label="Adresse" variant="filled" />
+        <TextInput label="Ville" variant="filled" />
+        <Button style={styles.btn} width={180} onPress={saveLocation}>
+          <Button.Label label="Valider" />
+        </Button>
+      </View>
     </ModalLayout>
   );
 };
@@ -16,5 +27,9 @@ const styles = createStyleSheet(() => ({
     flexDirection: "column",
     padding: 20,
     gap: 20,
+  },
+  btn: {
+    marginHorizontal: "auto",
+    marginTop: 20,
   },
 }));
