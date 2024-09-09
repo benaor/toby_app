dd79507c8871e35e01f2a5e676e8ae26e916ac14
Benaor <benjamingirard25@gmail.com>
2024-09-09 21:33:31 +0200

    refactor: ehanced toHumanDate

--- src/utils/dates/toHumanDate.ts
+++ src/utils/dates/toHumanDate.ts
@@ -1,16 +1,2 @@
-type HumanDate = `${string}/${string}/${string}`;
-
-export const toHumanDate = (date: Date): HumanDate => {
-  let jour: number | string = date.getDate();
-  let mois: number | string = date.getMonth() + 1;
-  let annee: number | string = date.getFullYear();
-
-  if (jour < 10) jour = "0" + jour;
-  if (mois < 10) mois = "0" + mois;
-
-  jour = jour.toString();
-  mois = mois.toString();
-  annee = annee.toString();
-
-  return `${jour}/${mois}/${annee}` satisfies HumanDate;
-};
+export const toHumanDate = (date: Date): string =>
+  date.toLocaleDateString("fr-FR");
