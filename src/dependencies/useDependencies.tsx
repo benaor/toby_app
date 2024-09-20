83bd281ccd383583d940d21a9b14077cd05e5da2
Benaor <benjamingirard25@gmail.com>
2024-09-20 14:02:24 +0200

    fix(storage): wording mistake on length

--- src/dependencies/useDependencies.tsx
+++ src/dependencies/useDependencies.tsx
@@ -4,6 +4,8 @@ import { Authenticator } from "@authentication/core/useCases/Authenticator.useca
 import { AuthProvider } from "@authentication/core/ports/AuthProvider.port";
 import { IStorage, TypedStorage } from "@shared/storage/storage.interface";
 import { Alerter } from "@shared/alerter/alerter.interface";
+import { InMemoryTypedStorage } from "@shared/storage/InMemoryStorage";
+import { SupabaseAuthProvider } from "@authentication/core/adapters/SupabaseAuthProvider";
 
 const DependenciesContext = createContext<Dependencies | null>(null);
 
@@ -15,32 +17,13 @@ export const DependenciesProvider = ({
   dependencies?: Partial<Dependencies>;
 }) => {
   const initialDeps = useMemo<Dependencies>(() => {
-    const authProvider: AuthProvider = {
-      login: jest.fn(),
-      logout: jest.fn(),
-      getSession: jest.fn(),
-      startAutoRefresh: jest.fn(),
-      stopAutoRefresh: jest.fn(),
-      onAuthStateChange: jest.fn(),
-    };
-
-    const storage: IStorage = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      removeItem: jest.fn(),
-      lenght: jest.fn(),
-    };
-
-    const typedStorage: TypedStorage = {
-      get: jest.fn(),
-      set: jest.fn(),
-      remove: jest.fn(),
-      getStorage: jest.fn(),
-    };
+    const storage: IStorage = localStorage;
+    const typedStorage: TypedStorage = new InMemoryTypedStorage(storage);
+    const authProvider: AuthProvider = new SupabaseAuthProvider(storage);
 
     const alerter: Alerter = {
-      success: jest.fn(),
-      error: jest.fn(),
+      success: alert,
+      error: alert,
     };
 
     const authenticator = new Authenticator(
--- src/modules/shared/storage/storage.interface.ts
+++ src/modules/shared/storage/storage.interface.ts
@@ -1,7 +1,7 @@
 import { StorageKey } from "./storage.keys";
 
 export interface IStorage
-  extends Pick<Storage, "getItem" | "setItem" | "removeItem" | "lenght"> {}
+  extends Pick<Storage, "getItem" | "setItem" | "removeItem" | "length"> {}
 
 export interface TypedStorage {
   get<T>(key: StorageKey): T | null;
